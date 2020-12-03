3c2bf751c9e7ff64fd7c33b7cbac37f936cdaf25
Mukela Zenith Mabunda <zenith.mabunda01@gmail.com>
2020-12-03 12:04:39 +0200

    current get fixed

--- Tests/Tests.cpp
+++ Tests/Tests.cpp
@@ -58,5 +58,58 @@ TEST_CASE("Test if cable can be created with no exceptions being thrown")
 
 TEST_CASE("Determine the corresponding current values")
 {
-    cableCurrent(InstallMethod::Method_1, 1.5, 2, freeAir::None)
+    //cableCurrent(InstallMethod method, float cableSize, unsigned int noOfCables, inFreeAir freeAir)
+    SUBCASE("Method 1")
+    {
+        cableCurrent current(InstallMethod::Method_1, 1.5, 2, inFreeAir::None);
+        CHECK(current.getCableCurrent()==14.5);
+    }
+
+    SUBCASE("Method 1")
+    {
+        cableCurrent current0(InstallMethod::Method_1, 1.5, 3, inFreeAir::None);
+        CHECK(current0.getCableCurrent()==13.5);
+    }
+
+    SUBCASE("Method 2")
+    {
+        cableCurrent current1(InstallMethod::Method_2, 1.5, 3, inFreeAir::None);
+        CHECK(current1.getCableCurrent()==15.5);
+    }
+
+    SUBCASE("Method 3")
+    {
+        cableCurrent current2(InstallMethod::Method_3, 1, 2, inFreeAir::None);
+        CHECK(current2.getCableCurrent()==15.5);
+    }
+
+    SUBCASE("Method 3")
+    {
+        cableCurrent current3(InstallMethod::Method_3, 120, 2, inFreeAir::None);
+        CHECK(current3.getCableCurrent()==330);
+    }
+
+    SUBCASE("Method 4")
+    {
+        cableCurrent current4(InstallMethod::Method_4, 300, 2, inFreeAir::None);
+        CHECK(current4.getCableCurrent()==635);
+    }
+
+    SUBCASE("Method 5, horizontal")
+    {
+        cableCurrent current5(InstallMethod::Method_5, 95, 3, inFreeAir::Horizontal);
+        CHECK(current5.getCableCurrent()==341);
+    }
+
+    SUBCASE("Mathod 5, Vertical")
+    {
+        cableCurrent current6(InstallMethod::Method_5, 95, 3, inFreeAir::Vertical);
+        CHECK(current6.getCableCurrent()==311);
+    }
+
+    SUBCASE("Method 5, Trefoil")
+    {
+        cableCurrent current7(InstallMethod::Method_5, 95, 3, inFreeAir::Trefoil);
+        CHECK(current7.getCableCurrent()==264);
+    }
 }
