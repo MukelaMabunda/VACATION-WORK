57a05939c5b91b2ed7f584f762358c354ee56e75
Mukela Zenith Mabunda <zenith.mabunda01@gmail.com>
2020-12-07 14:34:18 +0200

    create volateg drop calculator and control statements for AC and DC

--- Tests/voltageDrop.cpp
+++ Tests/voltageDrop.cpp
@@ -1,6 +1,29 @@
 #include "voltageDrop.h"
+#include <cmath>
+#include <cstdlib>
 
-voltageDrop::voltageDrop()
+voltageDrop::voltageDrop(double I, double L, double Xc, double Rc)
 {
-    //ctor
+    this -> I = I;
+    this -> L = L;
+    this -> Xc = Xc;
+    this -> Rc = Rc;
+
+    Zc = sqrt (pow(Xc,2) + pow(Rc,2));
+}
+
+double voltageDrop::voltageDropNum(std::string PhaseType)
+{
+    if (PhaseType == "1AC" || PhaseType == "DC")
+    {
+
+    }
+    else if (PhaseType == "3AC")
+    {
+
+    }
+    else
+    {
+        throw "WRONG PHASE TYPE USE EITHER: \n1AC - SINGLE PHASE\n3AC - THREE PHASE\nDC - DIRECT CURRENT";
+    }
 }
--- Tests/voltageDrop.h
+++ Tests/voltageDrop.h
@@ -1,15 +1,20 @@
 #ifndef VOLTAGEDROP_H
 #define VOLTAGEDROP_H
 
+#include <string>
 
 class voltageDrop
 {
 public:
-    voltageDrop();
-
-protected:
+    voltageDrop(double I, double L, double Xc, double Rc);
+    double voltageDropNum(std::string PhaseType);
 
 private:
+    double I = 0;
+    double L = 0;
+    double Xc= 0;
+    double Rc= 0;
+    double Zc= 0;
 };
 
 #endif // VOLTAGEDROP_H
